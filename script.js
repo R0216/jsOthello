d22db4c7a97c2686a09ced0b5f08c70609438fb2
R0216 <s1f102500247@iniad.org>
2026-03-09 15:34:33 +0900

    ターン制の設定

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const context = canvas.getContext("2d");
 const rect = canvas.getBoundingClientRect();
 context.scale(80, 80);
 
+let turn = 1;
 function drawBoard() {
     context.fillStyle = "green";
     context.fillRect(0, 0, 8, 8);
@@ -25,15 +26,35 @@ function drawBoard() {
 
 const board = Array.from({length: 8}, ()=> new Array(8).fill(0));
 console.table(board);
+board[3][3] = 1;
+board[3][4] = 2;
+board[4][3] = 2;
+board[4][4] = 1;
+
+function clickHandler(board, x, y){
+    if (turn === 1){
+        if(board[y][x] === 0){
+            board[y][x] = 1;
+            turn = 2;
+        }
+    } else {
+        if (board[y][x] === 0){
+            board[y][x] = 2;
+            turn = 1;
+        }
+    }
+    console.log(board)
+}
 
 window.addEventListener("click", (e) => {
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
+    const nx = Math.floor(x / 80);
+    const ny = Math.floor(y / 80);
 
 
-    console.log(`座標は`,{x, y})
-    console.log(Math.floor(x / 80))
-    console.log(Math.floor(y / 80))
+    console.log(`座標は`,{nx, ny})
+    clickHandler(board, nx, ny)
 })
 
 drawBoard();
