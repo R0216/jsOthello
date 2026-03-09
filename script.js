57c119b7989fda8f42f19306569cb752524ce1cf
R0216 <s1f102500247@iniad.org>
2026-03-09 15:03:17 +0900

    座標取得

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log("成功");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const rect = canvas.getBoundingClientRect();
+context.scale(80, 80);
+
+function drawBoard() {
+    context.fillStyle = "green";
+    context.fillRect(0, 0, 8, 8);
+    context.lineWidth = 0.05;
+    context.strokeStyle = "black";
+    for (let i = 0; i < 8; i++){
+        context.beginPath()
+        context.moveTo(i, 0);
+        context.lineTo(i, 8);
+        context.stroke();
+    }
+    for (let i = 0; i < 8; i++){
+        context.beginPath()
+        context.moveTo(0, i);
+        context.lineTo(8, i);
+        context.stroke();
+    }
+}
+
+
+const board = Array.from({length: 8}, ()=> new Array(8).fill(0));
+console.table(board);
+
+window.addEventListener("click", (e) => {
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+
+    console.log(`座標は`,{x, y})
+    console.log(Math.floor(x / 80))
+    console.log(Math.floor(y / 80))
+})
+
+drawBoard();
