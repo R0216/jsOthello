af2d3077f56763560fe8beb411890a722cfd2d53
R0216 <s1f102500247@iniad.org>
2026-03-09 23:35:04 +0900

    置けるかチェックする関数の作成

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ const context = canvas.getContext("2d");
 const rect = canvas.getBoundingClientRect();
 context.scale(80, 80);
 
+const dir = [
+    [-1, 0],
+    [-1, 1],
+    [-1, -1],
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [1, 1],
+    [1, -1],
+]
 let turn = 1;
+
 function drawBoard() {
     context.fillStyle = "green";
     context.fillRect(0, 0, 8, 8);
@@ -24,6 +35,8 @@ function drawBoard() {
 }
 
 
+
+
 const board = Array.from({length: 8}, ()=> new Array(8).fill(0));
 console.table(board);
 board[3][3] = 1;
@@ -31,18 +44,34 @@ board[3][4] = 2;
 board[4][3] = 2;
 board[4][4] = 1;
 
-function clickHandler(board, x, y){
-    if (turn === 1){
-        if(board[y][x] === 0){
-            board[y][x] = 1;
-            turn = 2;
+function canPut(board, x, y){
+    if (board[y][x] !== 0) return false;
+
+    for (const [dx, dy] of dir){
+        let nx = x + dx;
+        let ny = y + dy;
+        let hasOpponentPiece = false;
+
+        while(nx >= 0 && nx < 8 && ny >= 0 && ny < 8 && board[ny][nx] === 3 - turn){
+            nx += dx;
+            ny += dy;
+            hasOpponentPiece = true;
         }
-    } else {
-        if (board[y][x] === 0){
-            board[y][x] = 2;
-            turn = 1;
+        if(hasOpponentPiece && nx >= 0 && nx < 8 && ny >= 0 && ny < 8 && board[ny][nx] === 3 - turn){
+            return true
         }
     }
+    return false
+}
+
+function clickHandler(board, x, y){
+    if(canPut(board, x, y)){
+        if(board[y][x] === 0){
+            board[y][x] = turn;
+            turn = 3 - turn;
+        }
+    }
+    
     console.log(board)
 }
 
